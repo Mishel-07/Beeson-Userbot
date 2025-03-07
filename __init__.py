ebd19ef29523d13ce739023218404a4605d5d81f
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-03-07 16:25:26 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,16 @@
+from pyrogram import Client
+import os
+
+API_ID = int(os.environ.get('API_ID', ''))  
+API_HASH = os.environ.get('API_HASH', '')  
+SESSION = os.environ.get('SESSION', '')  
+
+
+TanjiroUb = Client(
+        "userbot",
+        session_string=SESSION,
+        api_id=API_ID,
+        api_hash=API_HASH,
+        plugins=dict(root="plugins") )
+
+    
