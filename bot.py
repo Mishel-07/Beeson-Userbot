f4acd0ce5e49011c773a3085804674d7c37779ec
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-03-09 15:32:03 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+from pyrogram import Client, idle
+import os
+
+API_ID = int(os.environ.get('API_ID', ''))  
+API_HASH = os.environ.get('API_HASH', '')  
+SESSION = os.environ.get('SESSION', '')  
+
+
+app = Client(
+        "mishal",
+        session_string=SESSION,
+        api_id=API_ID,
+        api_hash=API_HASH,
+        plugins=dict(root="plugins") 
+)
+
+async def start_bot():
+    await app.start()
+    await idle()
+
