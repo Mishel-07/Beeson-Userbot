4674db7f334b3c2166e7971c00fd1042b8864c31
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-03-09 15:39:18 +0530

    Delete tanjiro directory

--- tanjiro/__main__.py
+++
@@ -1,5 +0,0 @@
-from tanjiro import app
-
-
-if __name__ == "__main__":
-    app.run()
--- tanjiro/plugins/mics.py
+++
@@ -1,7 +0,0 @@
-from tanjiro import app
-from pyrogram import filters 
-
-@app.on_message(filters.command("alive") & filters.me)
-async def alive(me, message):
-    await message.reply("I'm alive")
-
