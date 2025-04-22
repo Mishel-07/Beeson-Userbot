1957e0f1fa9895286d7355f7f758578c737548bf
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-04-22 15:13:46 +0530

    Create __init__.py

---
+++ Beeson/__init__.py
@@ -0,0 +1,19 @@
+from pyrogram import Client, idle
+import os
+
+API_ID = int(os.environ.get('API_ID', ''))  
+API_HASH = os.environ.get('API_HASH', '')  
+SESSION = os.environ.get('SESSION', '')  
+
+
+app = Client(
+        "beeson",
+        session_string=SESSION,
+        api_id=API_ID,
+        api_hash=API_HASH,
+        plugins=dict(root="tanjiro") 
+)
+
+async def start_bot():
+    await app.start()
+    await idle()
