a6f796be32205e397e7068ae99f87a82842078fb
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-04-22 15:26:30 +0530

    Update __init__.py

--- beeson/__init__.py
+++ beeson/__init__.py
@@ -11,7 +11,7 @@ app = Client(
         session_string=SESSION,
         api_id=API_ID,
         api_hash=API_HASH,
-        plugins=dict(root="tanjiro") 
+        plugins=dict(root="beeson") 
 )
 
 async def start_bot():
